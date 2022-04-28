875d646d768cdfaa3ff24b2170d6dac274a8b46a
Ben Viggiano <viggiano.benjamin@gmail.com>
2022-04-28 11:36:51 -0700

    Updated README.md, fixed bug in utils.py

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# Capsulorrhexis Trajectories for Automated Surgical Training Feedback
+# 👁 Capsulorrhexis Trajectories for Automated Surgical Training Feedback 🥼
 
-## Contributors
+## Contributors 💻
 David Kuo MD, Ben Viggiano, Riya Sinha, Ben Ehlert
 
-## Gettting Started
+## Gettting Started ⚡️
 1) Clone the repository to your computer.<br>  
 
 2) Run the following command from inside the repository to set up the conda environment: <br>  `conda env create -f environment.yml` <br>  
 
-3) 
+3) Download our dataset from the following Google Drive link:
+`https://drive.google.com/drive/folders/1CEiRaF4CoqUOh7OzYxVoQGf39p3Hq2Qz?usp=sharing`
+
+    Save the dataset on your computer and get it's full filepath for the next step.
+
+4) Update the first line of the file `rhexis_config.txt` so that the first line contains:
+`DATA_LOCATION:<location>` <br>  where `<location>` is the full file path to the location of of the folder 'datasets'. (These file's changes are not tracked, so others using the repository can also specify their own paths)
+
+5) You are good to go! Check out `Detectron2_Rhexis.ipynb` 🙂
--- utils.py
+++ utils.py
@@ -1,3 +1,49 @@
-# Utility functions for the rhexis project
+"""
+A module of utility functions for the Rhexis project.
+"""
 
-def 
+from typing_extensions import assert_type
+import os
+
+
+def get_json_and_images(dataset_name: str):
+    """
+    This function returns the location of the the specified dataset.
+
+    Parameters:
+        dataset_name(str): A string specifying which dataset we would like to
+        collect the json and image locations of.
+        - 'train' to collect training dataset
+        - 'val' to collect validation set
+        - 'test' to collect test set
+
+    Returns:
+        json_loc(str): File path to JSON keypoint annotations
+        image_loc(str): File path to images directory
+
+
+    """
+
+    # Ensure the input is the correct type
+    assert_type(dataset_name, str)
+
+    # Ensure this input is one of the valid options
+    msg = f"'{dataset_name}' is an invalid option: Please use 'train', 'val', or 'test'"
+    assert dataset_name == 'train' or dataset_name == 'val' or dataset_name == 'test', msg
+
+    # Attempt to read in the config file to collect location
+    loc = ""
+    with open("rhexis_config.txt") as config_file:
+        # Read in file
+        data = config_file.read()
+        
+        # Collect data location string
+        loc = data.split(":")[1]
+
+    # Return the locations
+    json_loc = os.path.join(loc,f"{dataset_name}_set",
+        f"{dataset_name}_coco_keypoints_annotations.json")
+
+    image_loc = os.path.join(loc,f"{dataset_name}_set","images")
+
+    return json_loc, image_loc
