4523545147165fc88a35e73209db496a76f33ea3
Ben Viggiano <viggiano.benjamin@gmail.com>
2022-06-06 00:44:41 -0700

    Added moving average and drop row functions to utils

--- Trajectory_Classification/trajectory_smoothing.py
+++ Trajectory_Classification/trajectory_smoothing.py
@@ -6,13 +6,7 @@ import sys
 import glob
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 
-DATA_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls"
-REPO_LOC = "/content/drive/MyDrive/Projects/rhexis-trajectory"
 
 def normalize_coords(path_df, path_vid_size):
   height, width = path_vid_size
@@ -31,6 +25,7 @@ def apply_moving_average(path_df):
   for column in path_df:
     if column.endswith(("_x")) or column.endswith(("_y")):
       path_df[column] = path_df[column].rolling(5).mean()
+  
 
 def load_all_pulls(DATA_LOC:str):
   output_folder = os.path.join(DATA_LOC,"OUTPUT")
@@ -47,11 +42,10 @@ def load_all_pulls(DATA_LOC:str):
   path_vid_sizes = [get_video_resolution(video_size_df, csv.split('_fea')[0]) for csv in csvs]
 
   # Normalize all coords to size of image
-  for path, path_vid_size in zip(path_dfs, path_vid_sizes):
-    #path_dfs[i] = drop_rows(path)
-    #path_dfs[i] = apply_moving_average(path)
+  for i, (path, path_vid_size) in enumerate(zip(path_dfs, path_vid_sizes)):
     normalize_coords(path, path_vid_size)
-
+    path_dfs[i] = drop_rows(path)
+    path_dfs[i] = apply_moving_average(path)
   names = [csv.split('_fea')[0] for csv in csvs]
   return names, path_dfs, labels, path_vid_sizes
 
@@ -193,69 +187,3 @@ def angles_to_bins(angles, num_bins=20):
   """
   bins = [i for i in range(0, 181, 180//num_bins)] 
   return np.histogram(angles, bins)
-
-def stratified_split_data(X, y):
-  np.random.seed(13)
-  sss = StratifiedShuffleSplit(1, test_size=.2)
-  train_ind, test_ind = next(sss.split(X, y))
-  X_train, X_test = X[train_ind], X[test_ind]
-  y_train, y_test = y[train_ind], y[test_ind]
-  return X_train, X_test, y_train, y_test
-
-def make_custom_pipeline(clf, include_pca=False, num_components=0):
-  scaler = StandardScaler()
-  pca = PCA()
-  if include_pca:
-    return make_pipeline(scaler, pca, clf)
-  else:
-    return make_pipeline(scaler, clf)
-
-def run_pipeline_test():
-  X, y = np.stack([featurize_pull(pull, num_bins) for pull in path_dfs], axis=0), np.array(labels)
-  X_train, X_test, y_train, y_test = stratified_split_data(X, y)
-  search = GridSearchCV(make_custom_pipeline(clf, include_pca=True), param_grid, cv=5)
-  search.fit(X_train, y_train)
-
-def grid_search_with_bins(clf, param_grid, bin_range):
-  names, path_dfs, labels, sizes = load_all_pulls(DATA_LOC)
-  results = {}
-  for num_bins in bin_range:
-    X, y = np.stack([featurize_pull(pull, num_bins) for pull in path_dfs], axis=0), np.array(labels)
-    X_train, X_test, y_train, y_test = stratified_split_data(X, y)
-    search = GridSearchCV(make_custom_pipeline(clf, include_pca=True), param_grid, cv=5)
-    search.fit(X_train, y_train)
-    results[num_bins] = search
-  best_bin = get_best_gs_w_bins_nbins(results)
-  print(best_bin)
-  X, y = np.stack([featurize_pull(pull, best_bin) for pull in path_dfs], axis=0), np.array(labels)
-  X_train, X_test, y_train, y_test = stratified_split_data(X, y)
-  return {"results": results,
-          "best_search": get_best_gs_w_bins_search(results),
-          "best_score": get_best_gs_w_bins_score(results),
-          "best_params": get_best_gs_w_bins_params(results),
-          "X_train": X_train,
-          "X_test": X_test,
-          "y_train": y_train,
-          "y_test": y_test}
-
-def get_best_gs_w_bins_search(results):
-  return max(results.values(), key=lambda x: x.best_score_)
-
-def get_best_gs_w_bins_score(results):
-  return max(map(lambda x: x.best_score_, results.values()))
-
-def get_best_gs_w_bins_params(results):
-  pair = max(results.items(), key=lambda k_v: k_v[1].best_score_)
-  best_dict = pair[1].best_params_
-  best_dict["data__n_bins"] = pair[0]
-  return best_dict
-
-def get_best_gs_w_bins_nbins(results):
-  return max(results.items(), key=lambda k_v: k_v[1].best_score_)[0]
-
-
-
-
-
-
-
