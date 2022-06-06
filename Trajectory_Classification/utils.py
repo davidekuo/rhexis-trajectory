89c99f709e5ea0b7fa702a711d3443f6b8a4d9e1
Riya Sinha <riya.v.sinha@gmail.com>
2022-06-06 03:53:00 -0700

    Clean up trajectory classification notebook and grid search over kernelized pca hyperparameters

--- Trajectory_Classification/utils.py
+++ Trajectory_Classification/utils.py
@@ -9,7 +9,8 @@ import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.base import BaseEstimator, TransformerMixin
 
 DATA_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls"
 REPO_LOC = "/content/drive/MyDrive/Projects/rhexis-trajectory"
@@ -57,7 +58,7 @@ def load_all_pulls(DATA_LOC:str):
 
 
 
-def load_pull(filename):
+def load_pull(filename, header=None):
   '''Reads the coordinates from a pull file into a dataframe.
 
   Frames where a forceps was not detected are filtered out.
@@ -69,7 +70,7 @@ def load_pull(filename):
     A pandas dataframe of the coordinates where the forceps is present, sorted
       by frame (return Y, X)
   '''
-  data = pd.read_csv(filename)
+  data = pd.read_csv(filename, header=header) if header else pd.read_csv(filename)
   return data[data.key_L_x.notnull()].sort_values(by=['frame_num'])
 
 def get_video_resolution(video_resolution_df, filename):
@@ -85,16 +86,9 @@ def file_label(filename):
 
   Returns:
     A number representing the true class of the data:
-      0 = Junior Resident
-      1 = Senior Resident
-      2 = Expert
+      0 = Resident
+      1 = Expert
   '''
-  # if filename.startswith("Medi"):
-  #   return 0
-  # elif filename.startswith(("KY", "AC")):
-  #   return 1
-  # elif filename.startswith(("Cataract", "SQ")):
-  #   return 2
   if filename.startswith(("Medi","KY", "AC")):
     return 0
   elif filename.startswith(("Cataract", "SQ")):
@@ -257,9 +251,236 @@ def get_best_gs_w_bins_params(results):
 def get_best_gs_w_bins_nbins(results):
   return max(results.items(), key=lambda k_v: k_v[1].best_score_)[0]
 
+def convert_path_to_img_matrix(path_df, fill_val_w_frame = False):
+  '''
+  DEPRECATED. No longer in use since we are projecting onto a standardized pupil
+  matrix instead of getting the raw pixel path.
 
+  Converts a list of path coordinates to an image matrix.
+  In order to handle complexity with floats, the size of the matrix is 10x the
+  number of pixels, and each point is rounded to the nearest one-tenth pixel.
 
+  The matrix is also bounded such that the bottom-left pixel in the trajectory
+  is placed at (0, 0), by shifting all points by the minimum coordinate.
 
+  Additionally, there are two modes in which the matrix can be filled:
+    1. Boolean path matrix: Places a "1" in each position the forceps was
+         at any point in the trajectory
+    2. Frame number matrix: Places the frame number in which the forceps was
+         at a location. This may help with incorporate temporal quality.
 
+  Args:
+    path_df: the path coordinate dataframe to convert
+    fill_val_w_frame: Whether to fill in the frame number as the coordinate
+      value. Defaults to False.
 
+  Returns;
+    The 2D np array representing the trajectory
+  '''
+  lf_coords = path_df[["key_L_x", "key_L_y", "frame_num"]]
+  lf_coords["key_L_x"] = lf_coords["key_L_x"].subtract(lf_coords["key_L_x"].min()) 
+  lf_coords["key_L_y"] = lf_coords["key_L_y"].subtract(lf_coords["key_L_y"].min())
+  lf_coords = np.rint(lf_coords * 10).astype(int)
 
+  maxes = lf_coords.max()
+  arr = np.zeros((maxes[0], maxes[1]))
+  lf_np = lf_coords.to_numpy()
+  arr[lf_np[:, 0]-1, lf_np[:, 1]-1] = lf_np[:, 2] if fill_val_w_frame else 1
+  return arr
+
+def convert_path_to_std_pupil_matrix(path_df, size=None, fill_val_w_frame=False, verbose=False):
+  '''Maps a path to a normalized matrix representing position relative to the
+  center of a pupil.
+
+  Args:
+    path_df: The path coordinate dataframe to convert
+    vid_res: The resolution of the video the path was taken from, which helps
+      determine if any clipping of the pupil has occurred
+    size: If set, maps the pupil to a specified size matrix. If unset, uses
+      the pupil height and width for the matrix.
+    fill_val_w_frame: Whether to fill in the frame number as the coordinate
+      value. Defaults to False.
+  '''
+  # Determine the pupil size initial matrix
+  pupil_width = abs(path_df["pupil_right_x"] - path_df["pupil_left_x"]).mean()
+  pupil_height = abs(path_df["pupil_up_y"] - path_df["pupil_down_y"]).mean()
+  arr = np.zeros(size or (pupil_width, pupil_height))
+  arr_center_x, arr_center_y = (np.array(arr.shape) / 2).astype(int)
+
+  for i in range(len(path_df)):
+    # Calculate how much to translate forceps point to center pupil at center
+    # of matrix
+    translation_factor_x = path_df["pupil_left_x"].iloc[i]
+    translation_factor_y = path_df["pupil_up_y"].iloc[i]
+    if verbose:
+      print("=======================================================")
+      print("Translate: " + str((translation_factor_x, translation_factor_y)))
+
+    # Calculate scale factor for forceps
+    # Assume elliptical if tilted. Find max between both sides to ensure one side
+    # is not clipped
+    pcx = path_df["pupil_center_x"].iloc[i]
+    pcy = path_df["pupil_center_y"].iloc[i]
+    plx = path_df["pupil_left_x"].iloc[i]
+    prx = path_df["pupil_right_x"].iloc[i]
+    puy = path_df["pupil_up_y"].iloc[i]
+    pdy = path_df["pupil_down_y"].iloc[i]
+
+    radius_x = max(pcx - plx, prx - pcx)
+    radius_y = max(pcy - puy, pdy - pcy)
+    scale_factor_x = arr_center_x / radius_x
+    scale_factor_y = arr_center_y / radius_y
+    if verbose:
+      print("Scale: " + str((scale_factor_x, scale_factor_y)))
+
+    coord_l_x = int((path_df["key_L_x"].iloc[i] - translation_factor_x) * scale_factor_x)
+    coord_l_y = int((path_df["key_L_y"].iloc[i] - translation_factor_y) * scale_factor_y)
+
+    if verbose:
+      print("Coord: " + str((coord_l_y, coord_l_x)))
+      print("=======================================================")
+
+    if coord_l_x > 0 and coord_l_y > 0:
+      arr[coord_l_y, coord_l_x] = path_df["frame_num"].iloc[i] if fill_val_w_frame*10 else 1
+  return arr
+
+def get_pupil_std_data(matrix_size=None, fill_val_w_frame=False):
+  '''Loads data and performs conversion of paths to standardized pupil centered
+  matrices.
+
+  Args:
+    matrix_size: The matrix size to project onto for the conversion function
+    fill_val_w_frame: Whether to use the frame number as the value in the matrix.
+      Defaults to 1.
+  
+  Returns:
+    Tuple of matrix list and corresponding labels
+  '''
+  matrix_size = matrix_size or (100, 100)
+  DATA_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls/OUTPUT/"
+  files = os.listdir(DATA_LOC)
+  # VIDEO_SIZE_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls/pull_info.csv"
+  # video_size_df = pd.read_csv(VIDEO_SIZE_LOC)
+  path_dfs = [load_pull(f'{DATA_LOC}/{file}') for file in files]
+  # path_vid_sizes = [get_video_resolution(video_size_df, file.split('_fea')[0]) for file in files]
+  labels = [file_label(file) for file in files]
+  # path_matrices = [convert_path_to_std_pupil_matrix(df, res, matrix_size, fill_val_w_frame) for df, res in zip(path_dfs, path_vid_sizes)]
+  path_matrices = [convert_path_to_std_pupil_matrix(df, matrix_size, fill_val_w_frame) for df in path_dfs]
+  return np.stack(path_matrices, axis=0), np.array(labels)
+
+def get_pupil_std_data_traj(matrix_size=None, fill_val_w_frame=False):
+  '''Loads data and performs conversion of paths to standardized pupil centered
+  matrices.
+
+  Args:
+    matrix_size: The matrix size to project onto for the conversion function
+    fill_val_w_frame: Whether to use the frame number as the value in the matrix.
+      Defaults to 1.
+  
+  Returns:
+    Tuple of matrix list and corresponding labels
+  '''
+  matrix_size = matrix_size or (100, 100)
+  DATA_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls/OUTPUT_TRAJECTORIES/"
+  files = os.listdir(DATA_LOC)
+  # VIDEO_SIZE_LOC = "/content/drive/MyDrive/Rhexis/datasets/test_pulls/pull_info.csv"
+  # video_size_df = pd.read_csv(VIDEO_SIZE_LOC)
+  path_dfs = [load_pull(f'{DATA_LOC}/{file}', header=2) for file in files]
+  # path_vid_sizes = [get_video_resolution(video_size_df, file.split('_fea')[0]) for file in files]
+  labels = [file_label(file) for file in files]
+  path_matrices = [convert_path_to_std_pupil_matrix(df, matrix_size, fill_val_w_frame) for df in path_dfs]
+  return np.stack(path_matrices, axis=0), np.array(labels)
+
+class ImageFlatteningTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self):
+    return
+  def fit(self, X, y=None):
+    return self
+  def transform(self, X, y=None):
+    return np.reshape(X, (-1, 10000))
+
+def make_img_pipeline(clf, n_components=10):
+  return make_pipeline(ImageFlatteningTransformer(), 
+                       StandardScaler(),
+                       PCA(n_components=n_components, svd_solver="randomized", whiten=True),
+                       clf)
+
+def make_img_kernel_pipeline(clf, n_components=10):
+  return make_pipeline(ImageFlatteningTransformer(), 
+                       StandardScaler(),
+                       KernelPCA(n_components=n_components, kernel="rbf", gamma=None, fit_inverse_transform=True, alpha=0.1),
+                       clf)
+
+def grid_search_img(clf, kernelized, param_grid, X_train, y_train):
+  pipe = make_img_kernel_pipeline(clf) if kernelized else make_img_pipeline(clf)
+  search = GridSearchCV(pipe, param_grid, cv=4)
+  search.fit(X_train, y_train)
+  return search
+
+#####################################
+### DEPRECATED METHODS            ###
+#####################################
+
+def pad_matrices(path_matrices):
+  '''
+  DEPRECATED. No longer in use since we are projecting onto same-size matrices.
+
+  Pads the right and up sides of a matrix with 0s to make all matrices in a
+  list the same shape. Preserves the property that the bottom-left most point
+  in the trajectory will be at (0,0)
+
+  Args:
+    path_matrices: List of matrices to pad
+  
+  Returns:
+    A list of matrices of the same size
+  '''
+  max_x = max(map(lambda x: x.shape[0], path_matrices))
+  max_y = max(map(lambda x: x.shape[1], path_matrices))
+  for i, m in enumerate(path_matrices):
+    path_matrices[i] = np.pad(m, ((0, max_x-m.shape[0]), (0, max_y-m.shape[1])), mode='constant')
+    print(path_matrices[i].shape)
+  return path_matrices
+
+def featurize_trajectory(trajectory, label, transform_func=None):
+  """
+  DEPRECATED. No longer in use since we are using paths instead of trajectories.
+
+  Splits a trajectory by pulls and featurizes each pull. Each pull is assigned
+  the input label.
+
+  Args:
+    trajectory: The trajectory dataframe to split into featurized pulls
+    label: The label to assign each pull for the trajectory
+    transform_func: Custom transformation to use during featurization. Defaults
+        to `featurize_pull`.
+  
+  Return:
+    X: The feature rows
+    y: The labels for each row
+  """
+  transform_func = transform_func or featurize_pull
+  print(transform_func)
+  X = [transform_func(df) for val, df in trajectory.groupby('pull')]
+  y = np.repeat(label, len(X))
+  return np.array(X), y
+
+def featurize_trajectories(trajectories, transform_func=None):
+  """
+  DEPRECATED. No longer in use since we are using paths instead of trajectories.
+
+  Converts a list of trajectories into a feature matrix and label vector
+
+  Args:
+    trajectory: The trajctories list to split into featurized pulls
+    transform_func: Custom transformation to use during featurization. Defaults
+        to `featurize_pull`.
+  
+  Return:
+    X: The feature matrix
+    y: The label vector
+  """
+  featurized_trajectory_list = [featurize_trajectory(t, i, transform_func) for i, t in enumerate(trajectories)]
+  X = np.row_stack([x for x, y in featurized_trajectory_list])
+  y = np.concatenate([y for x, y in featurized_trajectory_list])
+  return X, y
